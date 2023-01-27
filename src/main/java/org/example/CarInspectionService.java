be2eeaa08d1afc021ba669209ea90de16218500d
lenaruppert <l.zi@gmx.de>
2023-01-27 12:27:05 +0100

    second iteration

--- src/main/java/org/example/CarInspectionService.java
+++ src/main/java/org/example/CarInspectionService.java
@@ -4,10 +4,9 @@ public class CarInspectionService {
     //private Car car;
     //prüfen ob ein Auto 4 Reifen hat
     public boolean hasFourTires(Car car) {
-        if (car.getNumberOfTires() == 4){
+        if (car.getNumberOfTires() == 4) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
@@ -24,11 +23,10 @@ public class CarInspectionService {
 
     //prüfen, ob das Auto einen seatBelt hat
 
-    public boolean hasSeatBelt(Car car){
-        if (car.isSeatBelt()){
+    public boolean hasSeatBelt(Car car) {
+        if (car.isSeatBelt()) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
@@ -36,18 +34,17 @@ public class CarInspectionService {
     public boolean hasThreeOrFiveTires(Car car) {
         if (car.getNumberOfTires() == 3 || car.getNumberOfTires() == 5) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
 
-    public boolean checkCar(Car car){
-        if(hasAirbag(car) && hasFourTires(car) && hasSeatBelt(car) && hasThreeOrFiveTires(car)){
+    public boolean checkCar(Car car) {
+        if (hasAirbag(car) && hasFourTires(car) && hasSeatBelt(car) && hasThreeOrFiveTires(car)) {
             return true;
-        }else  {
+        } else {
             return false;
         }
-        }
+    }
 
 }
