225ecb789369d279adaca83748f1ffb785c4075e
lenaruppert <l.zi@gmx.de>
2023-01-27 12:02:41 +0100

    Changes by tom

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,16 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+
+         Car audiA4 = new Car(4,5, true, true);
+         Car bmwM3 = new Car(4,3,true, true);
+
+        CarInspectionService newService = new CarInspectionService();
+
+        outPut(newService,audiA4);
+
+
+
     }
 
     public static void outPut(CarInspectionService newService,Car car){
