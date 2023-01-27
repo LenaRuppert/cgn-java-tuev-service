579589eaa36eca080ee7fb7125bdb2281404f6f1
lenaruppert <l.zi@gmx.de>
2023-01-27 12:01:55 +0100

    print Hello World

---
+++ src/test/java/CarinspectionServiceTest.java
@@ -0,0 +1,24 @@
+import org.example.Car;
+import org.example.CarInspectionService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CarinspectionServiceTest {
+
+    Car car1 = new Car(4, 4, true, true);
+    CarInspectionService carInspectionService1 = new CarInspectionService();
+
+    //prüfen ob ein Auto 4 Reifen hat
+    @Test
+    void hasFourTiresTest(){
+        Car car1 = new Car(4, 4, true, true);
+        boolean result = carInspectionService1.hasFourTires(car1);
+        Assertions.assertEquals(true, result);
+    }
+    @Test
+    void hasNotFourTiresTest() {
+        Car car2 = new Car(5, 4, true, true);
+        boolean result = carInspectionService1.hasFourTires(car2);
+        Assertions.assertEquals(false, result);
+    }
+}
